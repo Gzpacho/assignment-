8e141024289396fd68deff927b7ffc104e79925b
Gzpacho <144055885+Gzpacho@users.noreply.github.com>
2024-09-30 20:42:56 +0300

    Add files via upload

---
+++ ass/script.js
@@ -0,0 +1,4 @@
+document.getElementById('orderForm').addEventListener('submit', function(event) {
+    alert("Order submitted successfully to Eatings Restaurant!");
+    event.preventDefault();
+});
